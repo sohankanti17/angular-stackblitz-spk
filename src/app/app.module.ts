6f9d468b468ce818fece8f128c4500afd4d80675
sohankanti17 <sohan.kanti@bvicam.in>
2019-08-22 19:31:48 +0530

    navigation created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+
+import { CollapseModule, BsDropdownModule } from 'ngx-bootstrap';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent],
+  imports:      [ BrowserModule, BrowserAnimationsModule, FormsModule, CollapseModule.forRoot(), BsDropdownModule.forRoot() ],
+  declarations: [ AppComponent, NavigationComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
